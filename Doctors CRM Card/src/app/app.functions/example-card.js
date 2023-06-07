906030e54e455621034b4f15d920886f56b05928
VHubs <129496376+VHubs@users.noreply.github.com>
2023-06-07 13:34:30 +0200

    Update example-card.js

--- Doctors CRM Card/src/app/app.functions/example-card.js
+++ Doctors CRM Card/src/app/app.functions/example-card.js
@@ -6,15 +6,65 @@ exports.main = async (context = {}, sendResponse) => {
   const { firstname } = context.propertiesToSend;
 
   const tileOne = {
-    "type": "tile",
-    "content": [
-      {
-        "type": "heading",
-        "text": "Medical Data"
-      },
-    ]
-  };
-
+"type": "crm-card",
+"content": [
+{
+"type": "heading",
+"text": "Custom Card Example",
+},
+{
+"type": "alert",
+"title": "Alert: something you should be aware of",
+"variant": "error",
+"body": {
+"type": "text",
+"text": "click on the following url to either view or redirected to the document"
+}
+},
+{
+"type": "divider",
+"distance": "small"
+},
+{
+"type": "descriptionList",
+"items": [
+{
+"label": "Name:",
+"value": "John Smith"
+},
+{
+"label": "Address:",
+"value": "Am Postbahnhof 17, Berlin, Germany"
+},
+{
+"label": "Schedule:",
+"value": {
+"type": "text",
+"format": "markdown",
+"text": "[link to schedule](https://app.hubspot.com/l/docs/doc/platform/create-custom-crm-cards-with-projects#components)"
+}
+}
+]
+},
+{
+"type": "tag",
+"text": "Waiting for validation",
+"variant": "warning",
+"onClick": {
+"type": "SERVERLESS_ACTION_HOOK",
+"serverlessFunction": "exampleFunction"
+}
+},
+{
+"type": "button",
+"text": "Contact",
+"onClick": {
+"type": "SERVERLESS_ACTION_HOOK",
+"serverlessFunction": "exampleFunction"
+}
+}
+]
+};
   try {
     const { data } = await axios.get("https://zenquotes.io/api/random");
 
